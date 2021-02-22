6254f695ba7a0126b5e84b78bbcf99506f65c4ee
siown95 <siown95@naver.com>
2021-02-22 10:36:10 +0900

    commit

--- call_index.php
+++ call_index.php
@@ -190,7 +190,27 @@ function call_footer(){
 </div>';
 }
 function call_lnb(){
-
+    echo '<div id="nav-left" class="nav-left">
+		<div class="nav-left-tit"> 
+			<span>직무교육 안내</span>
+		</div>
+		<ul class="nav-left-lst">
+			<li><a href="#">해커스 HRD 소개</a></li>
+			<li><a href="#">사업주훈련</a></li>
+			<li><a href="#">근로자카드</a></li>
+			<li><a href="#">학습안내</a></li>
+			<li class="on"><a href="#">수강후기</a></li>
+		</ul>
+	</div>
+    <div id="content" class="content">
+		<div class="tit-box-h3">
+			<h3 class="tit-h3">수강후기</h3>
+			<div class="sub-depth">
+				<span><i class="icon-home"><span>홈</span></i></span>
+				<span>직무교육 안내</span>
+				<strong>수강후기</strong>
+			</div>
+		</div>';
 }
 function call_lnb_sub(){
 
